4b19cd5a0769e333327873aa24e7f053932a1680
Hugo <hugo.rubio.crespo@gmail.com>
2025-10-28 10:59:14 +0100

    conexionBD

---
+++ conecta.php
@@ -0,0 +1,21 @@
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$password = "";
+$database = "biblioteca";
+
+
+$conexion = new mysqli($servidor, $usuario,$password);
+
+if($conexion->connect_error){
+    die("Error de conexion: ". $conexion->connect_error);
+} else{
+    $sql ="CREATE DATABASE IF NOT EXISTS $database";
+    if($conexion->query($sql)){
+            echo "Base de Datos creada correctamente";  
+    } else{
+        echo "Error al crear";
+    }
+}
+$conexion->close();
+?>
